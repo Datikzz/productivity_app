f2a67fa91a94cd7ecb43d0d98c9a93bfc38e2f67
Dat Pham <dat_pham@epam.com>
2017-12-04 23:42:08 +0200

    upload again

--- src/app/router.js
+++ src/app/router.js
@@ -73,39 +73,44 @@ let context = {tasks: [
   {taskType: "task-hobby", priorityType: "priority-urgent", dateNum: 23, dateDay: "November", taskTitle: "Lorem ipsum sit amet",taskDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",priority: 3},
   {taskType: "task-hobby", priorityType: "priority-urgent", dateNum: null, dateDay: "Today", taskTitle: "Lorem ipsum sit amet",taskDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",priority: 3}]};
 
-//let tasksTempl = require('..//pages/tasks-list/tasks-list.hbs');
+let tasksTempl = require('../app/components/task-list/task-list.hbs');
 let settingsTempl = require('../app/components/settings-pomodoro/settings_pomodoros.hbs');
 
 let firstEntranceTempl = require('../app/components/first_entrance/first_entrance.hbs');
 let reportTempl = require('../app/components/report/report.hbs');
 let timerTempl = require('../app/components/timer/timer.hbs');
-  let body = document.getElementsByTagName("body")[0];
-  let headerTempl = require('../app/components/header/header.hbs');
+let body = document.getElementsByTagName("body")[0];
+let headerTempl = require('../app/components/header/header.hbs');
   body.innerHTML = headerTempl();
-// function pushHeader() {
-//   let body = document.getElementsByTagName("body")[0];
-//   let headerTempl = require('../app/components/header/header.hbs');
-//   body.innerHTML = headerTempl();
-// }
-// function pushL() {
-//   let tasksTempl = require('../app/components/task-list/task-list.hbs');
-//   let main = document.getElementsByTagName("main")[0];
-//   main.innerHTML = tasksTempl();
-// }
 
 let main = document.getElementsByTagName("main")[0];
-//let main = document.getElementsByTagName("main")[0];
-//main.innerHTML += tasksTempl(context);
-// main.innerHTML += settingsTempl();
-main.innerHTML += firstEntranceTempl();
-// main.innerHTML += reportTempl();
-// main.innerHTML += timerTempl();
-Router.add(/timer/, pushL);
 
-Router.navigate('/timer/');
-// Router.add(/settings-pomodoro/,() => {main.innerHTML = settingsTempl();});
-// Router.add(/timer/,() => {main.innerHTML = timerTempl();});
-// Router.add(/report/,() => {main.innerHTML = reportTempl();});
-// Router.add(/first-entrance/,() => {main.innerHTML = firstEntranceTempl();});
-// Router.navigate('/first-entrance');
 
+Router.add(/settings-pomodoro/,renderSettingsTempl);
+Router.add(/timer/,renderTimerTempl);
+Router.add(/report/,renderReportTempl);
+Router.add(/first-entrance/,renderFirstEntranceTempl);
+Router.add(/tasks-list/,renderTasksTempl);
+Router.navigate('/first-entrance/');
+function renderSettingsTempl(){
+  main.innerHTML += settingsTempl();
+}
+
+function renderFirstEntranceTempl(){
+  main.innerHTML += firstEntranceTempl();
+}
+
+function renderTimerTempl(){
+  main.innerHTML += timerTempl();
+}
+
+function renderReportTempl(){
+  main.innerHTML += reportTempl();
+}
+
+function renderTasksTempl(){
+  main.innerHTML += tasksTempl();
+}
+
+let skipBtn = document.querySelector('.skip-btn');
+skipBtn.addEventListener('click', Router.navigate('/tasks-list/'));
